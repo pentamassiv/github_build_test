059bc49ef177c110f13440ae5010e62d4e591491
pentamassiv <pentamassiv@posteo.de>
2022-04-08 17:05:00 +0200

    Change only to main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ fn main() {
     // Statements here are executed when the compiled binary is called
     let _x: u8 = random();
     // Print text to the console
-    println!("Hello World!");
+    println!("Hello World2!");
 }
