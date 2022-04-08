6431b30bfef0d8fca4fb61eed4d845f3fd458263
pentamassiv <pentamassiv@posteo.de>
2022-04-08 16:59:54 +0200

    Add test dependency

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use rand::prelude::*;
+
 fn main() {
     // Statements here are executed when the compiled binary is called
-
+    let _x: u8 = random();
     // Print text to the console
     println!("Hello World!");
 }
